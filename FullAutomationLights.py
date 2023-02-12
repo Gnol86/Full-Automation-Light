84a564ee118aa69affbdff7cf33ce4b391cbb816
Arnaud M <arnaud.marchot@icloud.com>
2023-02-12 13:37:28 +0100

    Fix bug

--- FullAutomationLights.py
+++ FullAutomationLights.py
@@ -3,7 +3,7 @@ import appdaemon.plugins.hass.hassapi as hass
 #
 # Light
 # @gnol86
-# https://github.com/Gnol86/FullAutomationLights
+# https://github.com/Gnol86/Full-Automation-Light
 #
 
 
@@ -21,14 +21,12 @@ class FullAutomationLights(hass.Hass):
         self.natural_lighting = self.init_natural_lighting()
         self.transitions = self.init_transitions()
         self.rooms = self.init_rooms()
-        
+
         # set all lights
         for room_name in self.rooms:
             self.set_is_light_on(room_name, "init")
         self.log("Initialized")
 
-    # check if all entities is available and return True or False
-        
     # add entity to list if not exists
     def add_entity(self, entity):
         if not entity in self.entities:
@@ -38,10 +36,10 @@ class FullAutomationLights(hass.Hass):
     def set_light(self, room_name, trigger):
         transition = self.transitions.get(trigger, 1)
         self.debug_log("{}: {} ({})".format(room_name, trigger, transition))
-        
+
         room = self.rooms[room_name]
         mode = ""
-        
+
         if room['is_light_on']:
             if 'scenes' in room:
                 for scene in room['scenes']:
@@ -49,32 +47,48 @@ class FullAutomationLights(hass.Hass):
                         mode = "scene"
                         entity = self.get_entity(scene['scene_entity'])
                         domain = scene['scene_entity'].split('.')[0]
-                        if domain == "script":
+                        if domain == "script" or transition == None:
                             entity.call_service("turn_on")
                         else:
-                            entity.call_service("turn_on", transition=transition)
-                        self.debug_log("ON - Scene : {}".format(scene['scene_entity']))
+                            entity.call_service(
+                                "turn_on", transition=transition)
+                        self.debug_log(
+                            "ON - Scene : {}".format(scene['scene_entity']))
                         break
             if mode == "" and self.natural_lighting and 'natural_lighting' in room:
                 for natural_lighting in room['natural_lighting']:
                     mode = 'natural_lighting'
                     brightness = self.natural_lighting['modes'][natural_lighting['name']]['brightness']
                     kelvin = self.natural_lighting['modes'][natural_lighting['name']]['kelvin']
-                    entity = self.get_entity(natural_lighting['lights_entity'])
+                    entity = self.get_entity(
+                        natural_lighting['lights_entity']) if 'lights_entity' in natural_lighting else self.get_entity(room['lights_entity'])
                     if natural_lighting['boost_brightness_pct'] != 0:
-                        brightness = int(brightness + (brightness / 100 * natural_lighting['boost_brightness_pct']))
+                        brightness = int(
+                            brightness + (brightness / 100 * natural_lighting['boost_brightness_pct']))
                         brightness = 255 if brightness > 255 else brightness
                         brightness = 1 if brightness < 1 else brightness
-                    entity.call_service("turn_on", brightness=brightness, kelvin=kelvin, transition=transition)
+                    if transition == None:
+                        entity.call_service(
+                            "turn_on", brightness=brightness, kelvin=kelvin)
+                    else:
+                        entity.call_service(
+                            "turn_on", brightness=brightness, kelvin=kelvin, transition=transition)
                 self.debug_log("ON - Natural lighting")
             if mode == "":
                 entity = self.get_entity(room['lights_entity'])
-                entity.call_service("turn_on", transition=transition)
+                if transition == None:
+                    entity.call_service("turn_on")
+                else:
+                    entity.call_service("turn_on", transition=transition)
                 self.debug_log("ON")
         else:
             self.debug_log("OFF")
             entity = self.get_entity(room['lights_entity'])
-            entity.call_service("turn_off", transition=self.transitions['off'])
+            if self.transitions['off'] == None:
+                entity.call_service("turn_off")
+            else:
+                entity.call_service(
+                    "turn_off", transition=self.transitions['off'])
         self.debug_log("")
 
     # init rooms config
@@ -88,66 +102,77 @@ class FullAutomationLights(hass.Hass):
             "luminance_limit": 10,
             "luminance_hysteresis": 0,
             "hight_luminance_off_light": False,
-            "occupancy" : True,
-            "low_light" : True,
-            "is_light_on" : False
-            }
+            "occupancy": True,
+            "low_light": True,
+            "is_light_on": False
+        }
         default_natural_lighting = {
             "name": "default",
             "boost_brightness_pct": 0
-            }
+        }
         rooms = {}
         if 'rooms' in self.args:
             for name in self.args['rooms']:
                 rooms[name] = default_room.copy()
-                rooms[name].update({k: v for k, v in self.args['rooms'][name].items() if k not in ['natural_lighting', 'scenes']})
+                rooms[name].update({k: v for k, v in self.args['rooms'][name].items(
+                ) if k not in ['natural_lighting', 'scenes']})
                 if not rooms[name]['lights_entity'] or not self.get_entity(rooms[name]['lights_entity']).exists():
                     self.log(f"'{name}' : 'lights_entity' not exists !!!")
                     del rooms[name]
                     continue
                 self.add_entity(rooms[name]['lights_entity'])
-                
+
                 if 'natural_lighting' in self.args['rooms'][name]:
                     rooms[name]['natural_lighting'] = []
                     if type(self.args['rooms'][name]['natural_lighting']) == list:
                         nb = 0
                         for natural_lighting in self.args['rooms'][name]['natural_lighting']:
-                            rooms[name]['natural_lighting'].append(default_natural_lighting.copy())
-                            rooms[name]['natural_lighting'][nb].update({k: v for k, v in natural_lighting.items()})
+                            rooms[name]['natural_lighting'].append(
+                                default_natural_lighting.copy())
+                            rooms[name]['natural_lighting'][nb].update(
+                                {k: v for k, v in natural_lighting.items()})
                             if not rooms[name]['natural_lighting'][nb]['name'] in self.natural_lighting['modes']:
                                 rooms[name]['natural_lighting'][nb]['name'] = 'default'
                             if 'natural_entity' in rooms[name]['natural_lighting'][nb]:
-                                self.add_entity(rooms[name]['natural_lighting'][nb]['natural_entity'])
+                                self.add_entity(
+                                    rooms[name]['natural_lighting'][nb]['natural_entity'])
                             else:
                                 rooms[name]['natural_lighting'][nb]['natural_entity'] = rooms[name]['lights_entity']
-                            
+
                             nb += 1
                     else:
-                        rooms[name]['natural_lighting'][0] = default_natural_lighting.copy()
+                        rooms[name]['natural_lighting'].append(
+                            default_natural_lighting.copy())
                         rooms[name]['natural_lighting'][0]['natural_entity'] = rooms[name]['lights_entity']
-                
+
                 if 'scenes' in self.args['rooms'][name] and type(self.args['rooms'][name]['scenes']) == list:
                     rooms[name]['scenes'] = self.args['rooms'][name]['scenes']
                     for scene in rooms[name]['scenes']:
                         if ('scene_entity' in scene
-                            and self.get_entity(scene['scene_entity']).exists()
-                            and 'scene_trigger_entity' in scene
-                            and self.get_entity(scene['scene_trigger_entity']).exists()
-                            and 'scene_trigger_value' in scene
-                            and (scene['scene_entity'].split('.')[0] == "script" or scene['scene_entity'].split('.')[0] == "scene")
+                                and self.get_entity(scene['scene_entity']).exists()
+                                and 'scene_trigger_entity' in scene
+                                and self.get_entity(scene['scene_trigger_entity']).exists()
+                                and 'scene_trigger_value' in scene
+                                and (scene['scene_entity'].split('.')[0] == "script" or scene['scene_entity'].split('.')[0] == "scene")
                             ):
-                            self.listen_state(self.callback_room, scene['scene_trigger_entity'], new=scene['scene_trigger_value'], room_name=name, trigger="scene")
-                            self.listen_state(self.callback_room, scene['scene_trigger_entity'], old=scene['scene_trigger_value'], room_name=name, trigger="scene")
-                    
+                            self.listen_state(
+                                self.callback_room, scene['scene_trigger_entity'], new=scene['scene_trigger_value'], room_name=name, trigger="scene")
+                            self.listen_state(
+                                self.callback_room, scene['scene_trigger_entity'], old=scene['scene_trigger_value'], room_name=name, trigger="scene")
+
                 if rooms[name]['occupancy_entity'] and self.get_entity(rooms[name]['occupancy_entity']).exists():
                     if rooms[name]['occupancy_off_delay'] > 0:
-                        self.listen_state(self.callback_room, rooms[name]['occupancy_entity'], new="on", room_name=name, trigger="occupancy")
-                        self.listen_state(self.callback_room, rooms[name]['occupancy_entity'], new="off", duration = rooms[name]['occupancy_off_delay'], room_name=name, trigger="occupancy")
+                        self.listen_state(
+                            self.callback_room, rooms[name]['occupancy_entity'], new="on", room_name=name, trigger="occupancy")
+                        self.listen_state(self.callback_room, rooms[name]['occupancy_entity'], new="off",
+                                          duration=rooms[name]['occupancy_off_delay'], room_name=name, trigger="occupancy")
                     else:
-                        self.listen_state(self.callback_room, rooms[name]['occupancy_entity'], room_name=name, trigger="occupancy")
+                        self.listen_state(
+                            self.callback_room, rooms[name]['occupancy_entity'], room_name=name, trigger="occupancy")
                 if rooms[name]['luminance_entity'] and self.get_entity(rooms[name]['luminance_entity']).exists():
-                    self.listen_state(self.callback_room, rooms[name]['luminance_entity'], room_name=name, trigger="low_light")
-                
+                    self.listen_state(
+                        self.callback_room, rooms[name]['luminance_entity'], room_name=name, trigger="low_light")
+
         self.debug_log(rooms)
         self.debug_log("Rooms Initialized")
         self.debug_log("")
@@ -160,15 +185,20 @@ class FullAutomationLights(hass.Hass):
     # set if light is on or off
     def set_is_light_on(self, room_name, trigger):
         if self.rooms[room_name]['occupancy_entity']:
-            self.rooms[room_name]['occupancy'] = True if self.get_state(self.rooms[room_name]['occupancy_entity']) in ["on","home",True,"true","True"] else False
+            self.rooms[room_name]['occupancy'] = True if self.get_state(
+                self.rooms[room_name]['occupancy_entity']) in ["on", "home", True, "true", "True"] else False
 
         if self.rooms[room_name]['luminance_entity']:
-            luminance_limit = self.int(self.rooms[room_name]['luminance_limit'])
+            luminance_limit = self.int(
+                self.rooms[room_name]['luminance_limit'])
             if self.rooms[room_name]['low_light']:
-                luminance_limit += self.int(self.rooms[room_name]['luminance_hysteresis'])
-            else: 
-                luminance_limit -= self.int(self.rooms[room_name]['luminance_hysteresis'])
-            self.rooms[room_name]['low_light'] = True if self.int(self.get_state(self.rooms[room_name]['luminance_entity'])) <= luminance_limit else False
+                luminance_limit += self.int(
+                    self.rooms[room_name]['luminance_hysteresis'])
+            else:
+                luminance_limit -= self.int(
+                    self.rooms[room_name]['luminance_hysteresis'])
+            self.rooms[room_name]['low_light'] = True if self.int(self.get_state(
+                self.rooms[room_name]['luminance_entity'])) <= luminance_limit else False
 
         old_is_light_on = self.rooms[room_name]['is_light_on']
         if not (self.rooms[room_name]['is_light_on'] and not self.rooms[room_name]['low_light']) or self.rooms[room_name]['hight_luminance_off_light'] or trigger == 'occupancy':
@@ -180,8 +210,8 @@ class FullAutomationLights(hass.Hass):
     # Initialize default transitions dictionary
     def init_transitions(self):
         transitions = {
-            'init': 0,
-            'occupancy': 1,
+            'init': None,
+            'occupancy': None,
             'low_light': 15,
             'scene': 10,
             'natural_lighting': 10,
@@ -215,18 +245,18 @@ class FullAutomationLights(hass.Hass):
             'min_elevation_for_kelvin': 0,
             'max_elevation_for_kelvin': 20
         }
-        
+
         # Check if natural_lighting exists in self.args
         if 'natural_lighting' not in self.args:
             self.debug_log("Natural Lighting Inactive")
             self.debug_log("")
             return False
-        
+
         self.debug_log("Init Natural Lighting...")
-        
+
         # Get natural lighting arguments from self.args
         nl_args = self.args['natural_lighting']
-        
+
         # Check if modes exist in natural lighting arguments
         if 'modes' in nl_args:
             modes = nl_args['modes']
@@ -234,26 +264,31 @@ class FullAutomationLights(hass.Hass):
             if type(modes) == list:
                 # Loop through modes and update the default values
                 for mode in modes:
-                    natural_lighting['modes'][mode['name']] = natural_lighting['modes']['default'].copy()
-                    natural_lighting['modes'][mode['name']].update({k: v for k, v in mode.items() if k in ['max_brightness', 'min_brightness', 'max_kelvin', 'min_kelvin']})
-        
+                    natural_lighting['modes'][mode['name']
+                                              ] = natural_lighting['modes']['default'].copy()
+                    natural_lighting['modes'][mode['name']].update({k: v for k, v in mode.items(
+                    ) if k in ['max_brightness', 'min_brightness', 'max_kelvin', 'min_kelvin']})
+
         # Get sun entity from natural lighting arguments
         sun_entity = nl_args.get('sun_entity', 'sun.sun')
         self.add_entity(sun_entity)
 
         # Update natural lighting with values from natural lighting arguments
-        natural_lighting.update({k: v for k, v in nl_args.items() if k in ['min_elevation_for_brightness', 'max_elevation_for_brightness', 'min_elevation_for_kelvin', 'max_elevation_for_kelvin']})
-        
+        natural_lighting.update({k: v for k, v in nl_args.items() if k in [
+                                'min_elevation_for_brightness', 'max_elevation_for_brightness', 'min_elevation_for_kelvin', 'max_elevation_for_kelvin']})
+
         # Get sun elevation state
-        natural_lighting['sun_elevation'] = self.int(self.get_state(sun_entity, attribute='elevation'))
-        
+        natural_lighting['sun_elevation'] = self.int(
+            self.get_state(sun_entity, attribute='elevation'))
+
         # Listen to changes in sun elevation state
-        self.listen_state(self.callback_sun_elevation, sun_entity, attribute='elevation')
+        self.listen_state(self.callback_sun_elevation,
+                          sun_entity, attribute='elevation')
         self.debug_log(f"Listen state of {sun_entity}")
-        
+
         # Set natural lighting
         natural_lighting = self.set_natural_lighting(natural_lighting)
-        
+
         self.debug_log(natural_lighting)
         self.debug_log("Natural Lighting Initialized")
         self.debug_log("")
@@ -261,7 +296,8 @@ class FullAutomationLights(hass.Hass):
 
     def callback_sun_elevation(self, entity, attribute, old, new, kwargs):
         self.natural_lighting['sun_elevation'] = self.int(new)
-        self.natural_lighting = self.set_natural_lighting(self.natural_lighting)
+        self.natural_lighting = self.set_natural_lighting(
+            self.natural_lighting)
         for room_name in self.rooms:
             if 'natural_lighting' in self.rooms[room_name] and self.rooms[room_name]['natural_lighting'] and self.rooms[room_name]['is_light_on']:
                 self.set_light(room_name, "natural_lighting")
@@ -274,16 +310,19 @@ class FullAutomationLights(hass.Hass):
             # Calculate the brightness value based on the sun elevation
             mode['brightness'] = self.scale(
                 natural_lighting['sun_elevation'],
-                (natural_lighting['min_elevation_for_brightness'], natural_lighting['max_elevation_for_brightness']),
+                (natural_lighting['min_elevation_for_brightness'],
+                 natural_lighting['max_elevation_for_brightness']),
                 (mode['min_brightness'], mode['max_brightness'])
             )
             # Calculate the kelvin value based on the sun elevation
             mode['kelvin'] = self.scale(
                 natural_lighting['sun_elevation'],
-                (natural_lighting['min_elevation_for_kelvin'], natural_lighting['max_elevation_for_kelvin']),
+                (natural_lighting['min_elevation_for_kelvin'],
+                 natural_lighting['max_elevation_for_kelvin']),
                 (mode['min_kelvin'], mode['max_kelvin'])
             )
-            self.debug_log(f"{name} - brightness: {mode['brightness']} - kelvin: {mode['kelvin']}")
+            self.debug_log(
+                f"{name} - brightness: {mode['brightness']} - kelvin: {mode['kelvin']}")
         self.debug_log("---")
         return natural_lighting
 
@@ -301,4 +340,5 @@ class FullAutomationLights(hass.Hass):
             return val
 
     def debug_log(self, message):
-        if self.debug: self.log(message)
+        if self.debug:
+            self.log(message)
